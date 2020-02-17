ef7e4bf22cd08745d94a4fde607337ea6734d9d3
masete <nicholasmasete72@gmail.com>
2020-02-17 10:54:15 +0300

    changes in the procfile and config file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ambs:app
+web: gunicorn run:app
--- ambs/models/ambs_db.py
+++ ambs/models/ambs_db.py
@@ -1,5 +1,5 @@
 import psycopg2
-import os
+from os import environ
 import ambs as ap
 from urllib.parse import urlparse
 from psycopg2.extras import RealDictCursor
