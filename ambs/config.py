1304049c90725ec2e831da8fe44bc2ea3acbe8b5
masete <nicholasmasete72@gmail.com>
2020-02-18 17:18:13 +0300

    postgres DB URL

--- ambs/config.py
+++ ambs/config.py
@@ -10,7 +10,7 @@ class ProductionConfig(BaseConfig):
     ENV = 'production'
     # DEBUG = False
     # TESTING = False
-    DATABASE_URI = 'postgres://qxunsdbokdvold:ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dartm9a0g4f4f0'
+    DATABASE_URL = 'postgres://qxunsdbokdvold:ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dartm9a0g4f4f0'
 
 env_config = dict(
     # development = DevelopmentConfig,
--- ambs/models/ambs_db.py
+++ ambs/models/ambs_db.py
@@ -123,7 +123,7 @@ class DatabaseConnection:
         try:
 
             if os.getenv("FLASK_ENV") == "production":
-                self.connection = psycopg2.connect(os.getenv("DATABASE_URL"), cursor_factory=RealDictCursor)
+                self.connection = psycopg2.connect("postgres://qxunsdbokdvold:ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dartm9a0g4f4f0", cursor_factory=RealDictCursor)
 
             elif os.getenv("FLASK_ENV") == "TESTING":
                 print('Connecting to test db')
