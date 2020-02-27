db3d5c912154c19696d6cfa2768a471564327fd5
masete <nicholasmasete72@gmail.com>
2020-02-27 08:32:02 +0300

    remove errors on charts

--- ambs/__init__.py
+++ ambs/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from .config import BaseConfig
 from flask_cors import CORS
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
-)
+# from flask_jwt_extended import (
+#     JWTManager, jwt_required, create_access_token,
+#     get_jwt_identity
+# )
 
 
 
 def create_app(config_class=BaseConfig):
     app = Flask(__name__)
     app.config.from_object(BaseConfig)
-    app.config['JWT_SECRET_KEY'] = 'mugisha-sylivin'  # Change this!
-    jwt = JWTManager(app)
+    # app.config['JWT_SECRET_KEY'] = 'mugisha-sylivin'  # Change this!
+    # jwt = JWTManager(app)
 
     CORS(app)
 
