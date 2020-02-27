0aed33a72539171dff82c2992aa598347586e964
masete <nicholasmasete72@gmail.com>
2020-02-27 08:14:26 +0300

    remove errors on charts

--- ambs/config.py
+++ ambs/config.py
@@ -26,21 +26,21 @@ class DevelopmentConfig(BaseConfig):
 #     TESTING = True
 
 
-class ProductionConfig(BaseConfig):
-    ENV = 'production'
-    # DEBUG = False
-    # TESTING = False
-    HOST = 'ec2-52-203-160-194.compute-1.amazonaws.com'
-    DATABASE = 'dartm9a0g4f4f0'
-    USER = 'qxunsdbokdvold'
-    PASSWORD = 'ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a'
-    DATABASE_URI = 'postgres://qxunsdbokdvold:ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dartm9a0g4f4f0'
-
+# class ProductionConfig(BaseConfig):
+#     ENV = 'production'
+#     # DEBUG = False
+#     # TESTING = False
+#     HOST = 'ec2-52-203-160-194.compute-1.amazonaws.com'
+#     DATABASE = 'dartm9a0g4f4f0'
+#     USER = 'qxunsdbokdvold'
+#     PASSWORD = 'ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a'
+#     DATABASE_URI = 'postgres://qxunsdbokdvold:ccd98e6f32a2d6b455dd941cf19bb1a5a12cb0b8d5ee8ff998c3c5d8a041322a@ec2-52-203-160-194.compute-1.amazonaws.com:5432/dartm9a0g4f4f0'
+#
 
 env_config = dict(
     development = DevelopmentConfig,
     # testing = TestingConfig,
-    production = ProductionConfig
+    # production = ProductionConfig
 )
 
 
